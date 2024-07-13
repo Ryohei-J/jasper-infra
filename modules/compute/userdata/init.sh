b4bd8a9460d1992883e8d55fe82616b9730ff707
Ryohei Jin <ad.tgib.tsr@gmail.com>
2024-07-13 16:03:39 +0900

    init.shを修正

--- modules/compute/userdata/init.sh
+++ modules/compute/userdata/init.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-dnf update 
-
-dnf install nginx -y 
+dnf update  
 
 dnf install nginx -y 
 systemctl enable nginx
 systemctl start nginx
-firewall-cmd --permanent --add-port=80/tcp
+
+firewall-offline-cmd --add-port=80/tcp
 firewall-cmd --reload
